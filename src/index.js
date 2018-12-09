ea41b3f7f6a6ad510f455b71399d7b7c4151a0c4
NadiaAiraf <aidanfaria@gmail.com>
2018-12-09 20:43:53 +0000

    simple setup of the page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class AnagramFinder extends React.Component {
+
+
+  render() {
+    return(
+      <form>
+        <label>
+          <textarea placeholder={'enter word here'} />
+        </label>
+        <ul>
+          // Where my list of anagrams will be
+        </ul>
+      </form>
+    )
+  }
+}
+
+ReactDOM.render(
+  <AnagramFinder />,
+  document.getElementById('root')
+)
