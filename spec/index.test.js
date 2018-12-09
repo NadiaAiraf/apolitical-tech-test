42b17824375e720562b1bcdc003a44ef3b76c729
NadiaAiraf <aidanfaria@gmail.com>
2018-12-09 21:21:53 +0000

    test for returning all the words in the text file

--- spec/index.test.js
+++ spec/index.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { shallow } from '../src/enzyme'
 
 import AnagramFinder from '../src/index.js';
+import returnWords from '../src/index.js'
 
-describe('AnagramFinder', () => {
-  it('should render a form html element', () => {
-    const wrapper = shallow(<AnagramFinder />)
-    expect(wrapper.find('form').length).toEqual(1)
+describe('returnWords', () => {
+  it('returns a an array of words from the txt file', () => {
+    const array = returnWords();
+    expect(array[0]).toEqual('A')
   })
 })
+
+// describe('AnagramFinder', () => {
+//   it('should render a form html element', () => {
+//     const wrapper = shallow(<AnagramFinder />)
+//     expect(wrapper.find('form').length).toEqual(1)
+//   })
+// })
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function readString () {
+function returnWords () {
         var request = new XMLHttpRequest();
         request.open("GET", "./wordlist.txt", false);
         request.send();
